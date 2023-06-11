470819ea37b92180e6e6d0f7a8ac518dbab4fcf4
Ethan <ethan@bombshelltechnologies.com>
2023-06-10 21:35:32 -0500

    fixes trpc + clerk error

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,17 +4,18 @@ import { NextResponse } from "next/server";
 export default withClerkMiddleware(() => {
   return NextResponse.next();
 });
- 
-export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next
-     * - static (static files)
-     * - favicon.ico (favicon file)
-     * - public folder
-     */
-    "/((?!static|.*\\..*|_next|favicon.ico).*)",
-    "/",
-  ],
-}
+
+export const config = { matcher: [ "/(.*?trpc.*?|(?!static|.*\\..*|_next|favicon.ico).*)", "/" ], };
+// export const config = {
+//   matcher: [
+//     /*
+//      * Match all request paths except for the ones starting with:
+//      * - _next
+//      * - static (static files)
+//      * - favicon.ico (favicon file)
+//      * - public folder
+//      */
+//     "/((?!static|.*\\..*|_next|favicon.ico).*)",
+//     "/",
+//   ],
+// }
