ecee11285b337bad19f6ac90734e048718855485
Ethan <ethan@bombshelltechnologies.com>
2023-06-12 07:01:24 -0500

    updates env.mjs to validate all env variables

--- src/env.mjs
+++ src/env.mjs
@@ -4,8 +4,19 @@ import { z } from "zod";
  * Specify your server-side environment variables schema here. This way you can ensure the app isn't
  * built with invalid env vars.
  */
+
 const server = z.object({
   DATABASE_URL: z.string().url(),
+  DATABASE_HOST: z.string().min(1),
+  DATABASE_PORT: z.string().min(1),
+  DATABASE_USER: z.string().min(1),
+  DATABASE_PASSWORD: z.string().min(1),
+  DATABASE_SCHEMA: z.string().min(1),
+  NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(22),
+  CLERK_SECRET_KEY: z.string().min(22),
+  CLERK_USER_EVENT_WEBHOOK_SECRET: z.string().min(26),
+  UPSTASH_REDIS_REST_URL: z.string().url(),
+  UPSTASH_REDIS_REST_TOKEN: z.string().min(24),
   NODE_ENV: z.enum(["development", "test", "production"]),
 });
 
@@ -18,7 +29,7 @@ const client = z.object(
     {
       // NEXT_PUBLIC_CLIENTVAR: z.string().min(1),
     }
-  ),
+  )
 );
 
 /**
@@ -27,8 +38,20 @@ const client = z.object(
  *
  * @type {Record<keyof z.infer<typeof server> | keyof z.infer<typeof client>, string | undefined>}
  */
+
 const processEnv = {
   DATABASE_URL: process.env.DATABASE_URL,
+  DATABASE_HOST: process.env.DATABASE_HOST,
+  DATABASE_PORT: process.env.DATABASE_PORT,
+  DATABASE_USER: process.env.DATABASE_USER,
+  DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
+  DATABASE_SCHEMA: process.env.DATABASE_SCHEMA,
+  NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+    process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+  CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+  CLERK_USER_EVENT_WEBHOOK_SECRET: process.env.CLERK_USER_EVENT_WEBHOOK_SECRET,
+  UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
+  UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
   NODE_ENV: process.env.NODE_ENV,
   // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
 };
@@ -60,7 +83,7 @@ if (!skip) {
   if (parsed.success === false) {
     console.error(
       "❌ Invalid environment variables:",
-      parsed.error.flatten().fieldErrors,
+      parsed.error.flatten().fieldErrors
     );
     throw new Error("Invalid environment variables");
   }
@@ -74,7 +97,7 @@ if (!skip) {
         throw new Error(
           process.env.NODE_ENV === "production"
             ? "❌ Attempted to access a server-side environment variable on the client"
-            : `❌ Attempted to access server-side environment variable '${prop}' on the client`,
+            : `❌ Attempted to access server-side environment variable '${prop}' on the client`
         );
       return target[/** @type {keyof typeof target} */ (prop)];
     },
