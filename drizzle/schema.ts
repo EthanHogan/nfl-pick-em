b757fe0a1e7d0c61780dd5098d937a4fe18d5c1a
EthanHogan <ethan.hogan7@gmail.com>
2023-09-03 10:34:56 -0500

    adds initial version of schema

--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -1,4 +1,4 @@
-import { mysqlTable, serial, datetime, varchar } from "drizzle-orm/mysql-core";
+import { mysqlTable, serial, datetime, varchar, boolean, } from "drizzle-orm/mysql-core";
 import { type InferModel, relations, sql } from "drizzle-orm";
 
 export const user = mysqlTable("user", {
@@ -41,3 +41,75 @@ export const messageRelations = relations(message, ({ one }) => ({
     references: [user.id],
   }),
 }));
+
+export const teams = mysqlTable("teams", {
+  id: serial("id").primaryKey().notNull(),
+  name: varchar("name", { length: 256 }),
+});
+export type Team = InferModel<typeof teams, "select">;
+export type NewTeam = InferModel<typeof teams, "insert">;
+export const teamRelations = relations(teams, ({ many }) => ({
+  matchupsHomeTeam: many(matchups, {relationName: "homeTeam"}),
+  matchupsAwayTeam: many(matchups, {relationName: "awayTeam"}),
+  matchupsWinningTeam: many(matchups, {relationName: "winningTeam"}),
+  picks: many(picks),
+}));
+
+export const matchups = mysqlTable("matchups", {
+  id: serial("id").primaryKey().notNull(),
+  startDate: datetime("startDate", { mode: "date" }).notNull(),
+  homeTeamId: varchar("homeTeamId", { length: 256 }).notNull(),
+  awayTeamId: varchar("awayTeamId", { length: 256 }).notNull(),
+  winningTeamId: varchar("winningTeamId", { length: 256 }),
+  isTie: boolean("isTie").notNull().default(false),
+});
+export type Matchup = InferModel<typeof matchups, "select">;
+export type NewMatchup = InferModel<typeof matchups, "insert">;
+export const matchupRelations = relations(matchups, ({ one }) => ({
+  homeTeam: one(teams, {
+    relationName: "homeTeam",
+    fields: [matchups.homeTeamId],
+    references: [teams.id],
+  }),
+  awayTeam: one(teams, {
+    relationName: "awayTeam",
+    fields: [matchups.awayTeamId],
+    references: [teams.id],
+  }),
+  winningTeam: one(teams, {
+    relationName: "winningTeam",
+    fields: [matchups.winningTeamId],
+    references: [teams.id],
+  }),
+}));
+
+export const picks = mysqlTable("picks", {
+  id: serial("id").primaryKey().notNull(),
+  userId: varchar("userId", { length: 256 }).notNull(),
+  matchupId: varchar("matchupId", { length: 256 }).notNull(),
+  teamId: varchar("teamId", { length: 256 }).notNull(),
+});
+export type Pick = InferModel<typeof picks, "select">;
+export type NewPick = InferModel<typeof picks, "insert">;
+export const pickRelations = relations(picks, ({ one }) => ({
+  user: one(user, {
+    fields: [picks.userId],
+    references: [user.id],
+  }),
+  matchup: one(matchups, {
+    fields: [picks.matchupId],
+    references: [matchups.id],
+  }),
+  team: one(teams, {
+    fields: [picks.teamId],
+    references: [teams.id],
+  }),
+}));
+// export const picksView = mysqlView("picksView").as((qb) => {
+//   return qb.select()
+//     .from(picks)
+//     .innerJoin(matchups, eq(matchups.id, picks.matchupId))
+//     .innerJoin(teams, eq(teams.id, picks.teamId))
+//     .innerJoin(user, eq(user.id, picks.userId))
+// });
+
